4eb6eec87c134fd2715f3cfdcebebe80908bea3f
yongsin0129 <yongsin0129@gmail.com>
2022-10-30 13:33:55 +0800

    feat : add hashTable

---
+++ 04.dataStructure/hashtable.js
@@ -0,0 +1,73 @@
+class HashTable {
+  constructor(size) {
+    this.size = size;
+    this.table = [];
+    for (let i = 0; i < this.size; i++) {
+      this.table.push([]);
+    }
+  }
+
+  // parse string to integer
+  parse(str) {
+    let result = 0;
+    for (let i = 0; i < str.length; i++) {
+      result = str.charCodeAt(i); // MDN
+    }
+    return result % this.size;
+  }
+
+  // division method
+  hash1(key) {
+    return key % this.size;
+  }
+
+  // multiplication method
+  hash2(key) {
+    let parsedKey = 0;
+    if (typeof key !== "number") {
+      parsedKey = this.parse(key);
+    } else {
+      parsedKey = key;
+    }
+    let A = (Math.sqrt(5) - 1) / 2;
+    return Math.floor(this.size * ((parsedKey * A) % 1));
+  }
+
+  set(key, value) {
+    let index = this.hash2(key);
+    this.table[index].push({ key, value });
+  }
+
+  get(key) {
+    let index = this.hash2(key);
+    for (let i = 0; i < this.table[index].length; i++) {
+      if (this.table[index][i].key == key) {
+        return this.table[index][i];
+      }
+    }
+    return null;
+  }
+
+  printAll() {
+    console.log(this.table);
+  }
+}
+
+let myHashTable = new HashTable(6);
+// CSS
+myHashTable.set("white", "#FFFFFF");
+myHashTable.set("magenta", "#FF00FF");
+myHashTable.set("red", "#FF0000");
+
+// console.log(myHashTable.get("white").value);
+myHashTable.printAll();
+
+
+// objects and arrays are hashed in Javascript
+// let Harry = {
+//   fullname: "Harry Potter",
+//   age: "15",
+//   school: "Hogwarts",
+// };
+
+// console.log(Harry["school"]);
