686e4bc2ce33ea93511a0f8b168d68c10e63a67e
yongsin0129 <yongsin0129@gmail.com>
2023-03-23 18:19:32 +0800

    refactor : 優化 leetcode 409

--- javascript-leetcode/easy-409Longest-Palindrome.js
+++ javascript-leetcode/easy-409Longest-Palindrome.js
@@ -5,33 +5,41 @@
 var longestPalindrome = function (s) {
   let number = 0
   let counter = {}
+
   // 使用 counter 計算每個字母出現的次數
   for (let index = 0; index < s.length; index++) {
     if (counter[s[index]] === undefined) { counter[s[index]] = 1 }
     else counter[s[index]]++
   }
 
+  // 針對字數的出現次數分成雙數及奇數
+  let oddNumberArray = []
   Object.values(counter).forEach((value) => {
     // 雙數一定可以組成 Palindrome，所以全部加總
     if (value % 2 === 0) number += value
+
+    // 單數有例外情況，需分流處理
+    else oddNumberArray.push(value)
   })
 
-  // 計算單數的相加結果
-  number += countTotalOddNumber(counter)
+  // 把雙數的結果與單數的結果相加
+  number += countTotalOddNumber(oddNumberArray)
 
   // return 結果
   return number
 
+
   // 單數只有最大的可以組成 Palindrome，剩下的都要減 1變成雙數，最後將所有數字加起來
-  function countTotalOddNumber (counter) {
-
-    const oddNumberArray = Object.values(counter).filter((value) => value % 2 !== 0)
-    // 沒有任何奇數，直接return 0
-    if (oddNumberArray.length === 0) return 0
-
-    // 存在奇數，先找最大值，剩下的全部 -1 後相加 (有可能會有最大值重複的問題，所以設計一個 flag)
-    const maxOddNumber = Math.max(...oddNumberArray)
+  function countTotalOddNumber (oddNumberArray) {
     let totalOddNumber = 0
+
+    // 沒有任何奇數，直接 return
+    if (oddNumberArray.length === 0) return totalOddNumber
+
+    // 存在奇數，先找最大值，剩下的全部 -1 變雙數之後再相加
+    const maxOddNumber = Math.max(...oddNumberArray)
+
+    //  (有可能會有最大值重複的問題，所以設計一個 flag)
     let isMaxOddNumberOccupied = false
     oddNumberArray.forEach((value, index) => {
       if (value === maxOddNumber && isMaxOddNumberOccupied === false) isMaxOddNumberOccupied = true
