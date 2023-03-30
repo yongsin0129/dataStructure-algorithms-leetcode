53d554e3f2e1fb78b7bf9be86fdc62d4e7a3915e
yongsin0129 <yongsin0129@gmail.com>
2023-03-30 22:26:58 +0800

    feat : add leetcode 013

---
+++ javascript-leetcode/.vscode/easy-13Roman-to-Integer.js
@@ -0,0 +1,73 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  let result = 0
+  // 先設計一個 dict 
+  const romanNumber = { 'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+  // string loop
+  for (let i = 0; i < s.length; i++) {
+    // 如果 index 不是最後一個數，且羅馬數字小於下一個 index的羅馬數字，則減去
+    if (i < s.length - 1 && romanNumber[s[i]] < romanNumber[s[i + 1]]) result -= romanNumber[s[i]]
+    // 做加法
+    else result += romanNumber[s[i]]
+  }
+
+  return result
+}
+
+/********************************************************************************
+*
+  Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+For example, 2 is written as II in Roman numeral, just two ones added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+ 
+
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+*
+*********************************************************************************/
+
+const s1 = "III" // 3
+console.log(romanToInt(s1))
+const s2 = "LVIII" //58
+console.log(romanToInt(s2))
+const s3 = "MCMXCIV" // 1994
+console.log(romanToInt(s3))
