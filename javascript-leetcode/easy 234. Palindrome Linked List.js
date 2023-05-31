8dcfab59fff7d0a3dec6a6a5c2c6c0367d9b15cd
yongsin0129Home <yongsin0129@gmail.com>
2023-05-31 18:46:00 +0800

    add leetcode 234

---
+++ javascript-leetcode/easy 234. Palindrome Linked List.js
@@ -0,0 +1,125 @@
+/********************************************************************************
+*
+Given the head of a singly linked list, return true if it is a 
+palindrome
+ or false otherwise.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,2,1]
+Output: true
+Example 2:
+
+
+Input: head = [1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [1, 105].
+0 <= Node.val <= 9
+ 
+
+Follow up: Could you do it in O(n) time and O(1) space?
+*
+*********************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function (head) {
+  let forwardArray = []
+  let node = head
+
+  while (node) {
+    let value = node.val
+    forwardArray.push(value)
+    node = node.next
+  }
+
+  let r = forwardArray.length - 1
+  let l = 0
+
+  while (l <= r) {
+    if (forwardArray[l] !== forwardArray[r]) return false
+    l++
+    r--
+  }
+  return true
+}
+
+
+/********************************************************************************
+*
+先找到 mid 
+再將 mid 的后半部分反轉
+
+ex : [1,2,3,3,2,1] = > [1 -> 2 -> 3][3 <- 2 <- 1]
+
+再從左右兩邊開始比較
+
+var isPalindrome = function(head) {
+    const mid = getMiddleNode(head);
+    const afterMid = reverseList(mid.next)
+    let leftSide = head;
+    let rightSide = afterMid;
+
+    let res = true;
+
+    while(rightSide) {
+        if (rightSide.val !== leftSide.val) {
+            res = false;
+            break;
+        };
+
+        rightSide = rightSide.next;
+        leftSide = leftSide.next;
+    }
+    
+    reverseList(afterMid)
+
+    return res;
+};
+
+function reverseList(head) {
+    if (head === null) return null
+
+    let prev = null;
+    let curr = head;
+    let nextTemp;
+
+    while (curr !== null) {
+        nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+    }
+
+    return prev;
+}
+
+function getMiddleNode(head) {
+    let fast = head;
+    let slow = head;
+
+    while (fast.next && fast.next.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+
+    return slow;
+}
+*
+*********************************************************************************/
--- javascript-leetcode/easy 9Palindrome-Number.js
+++ javascript-leetcode/easy 9Palindrome-Number.js
@@ -27,3 +27,61 @@ const isPalindrome = function (x) {
 // }
 
 isPalindrome(121)
+
+
+/********************************************************************************
+*
+// 第三種解法
+
+var isPalindrome = function (x) {
+  var reverse = 0
+  var copy = x
+
+  //The loop break when the copy of original number becomes zero
+  //Also negative number cannot be a palindrome
+  while (copy > 0) {
+    const digit = copy % 10
+    reverse = reverse * 10 + digit
+    copy = Math.floor(copy / 10)
+  }
+
+  return reverse == x
+}
+
+original number: 543
+reverse number: 0
+
+//Get the last digit of the original number
+original % 10 = 543 % 10 = 3
+//Put this digit as the last one in the reverse number
+reverse * 10 + digit = 0 * 10 + 3 = 0 + 3 = 3
+reverse: 3
+//Remove this digit from the original number
+original / 10 = 543 / 10 = 54.3
+~~54.3 = 54
+original: 54
+
+//Repeat
+original % 10 = 54 % 10 = 4
+reverse * 10 + digit = 3 * 10 + 4 = 30 + 4 = 34
+reverse: 34
+original / 10 = 54 / 10 = 5.4
+~~5.4 = 5
+original: 5
+
+//Repeat
+original % 10 = 5 % 10 = 5
+reverse * 10 + digit = 34 * 10 + 5 = 340 + 5 = 345
+reverse: 345
+original / 10 = 5 / 10 = 0.5
+~~0.5 = 0
+original: 0
+
+input: 543
+output: 345
+So if the reverse number is equal to the original number, then it is a palindrome
+
+345 != 543 //not palindrome
+272 == 272 //is palindrome
+*
+*********************************************************************************/
