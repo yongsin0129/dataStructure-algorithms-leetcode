f826f1819f53297e5ab33a68684361c0d9833ed5
yongsin0129Home <yongsin0129@gmail.com>
2023-06-09 19:29:46 +0800

    add leetcode 572

---
+++ javascript-leetcode/easy 572. Subtree of Another Tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+  if (root === null) return false
+  if (JSON.stringify(root) === JSON.stringify(subRoot)) return true
+
+  return isSubtree(root.right, subRoot) || isSubtree(root.left, subRoot)
+};
+/********************************************************************************
+*
+Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+Example 2:
+
+
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+*
+*********************************************************************************/
